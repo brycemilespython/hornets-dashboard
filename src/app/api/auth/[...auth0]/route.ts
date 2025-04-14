e344a285f40ed726b3378f6e0fc263d567f29b28
Bryce Miles <bryce.miles@musco.com>
2025-04-14 18:32:12 -0400

    Update route.ts

--- src/app/api/auth/[...auth0]/route.ts
+++ src/app/api/auth/[...auth0]/route.ts
@@ -1,7 +1,8 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
+import { NextRequest } from 'next/server';
 
 export const GET = handleAuth({
-  onError(req, error) {
+  onError(req: NextRequest, error: Error) {
     console.error('Auth0 Error:', error);
     return Response.redirect('/login?error=true');
   }
